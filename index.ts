b750e2e16b417fb9c009e44a127af23921637f66
9bytes <9bytes@gmail.com>
2022-03-16 00:58:03 +0530

    Full width styles

--- index.ts
+++ index.ts
@@ -2,17 +2,18 @@
 import Table from './src/Table';
 import './stts-style.css';
 
-// Write TypeScript
+// TODO: Consumer example will be moved to examples section
 Table({
   target: 'stts-container',
   columns: [
-    { headerText: 'First Name', valueProvider: 'firstName' },
     {
       headerText: 'Last Name',
       valueProvider: (item, column) => {
         return `<input type="checkbox" onchange="alert('${item['firstName']}')">`;
       },
     },
+    { headerText: 'First Name', valueProvider: 'firstName' },
+    { headerText: 'Last Name', valueProvider: 'lastName' },
   ],
   data: [{ firstName: 'Mahesh', lastName: 'Varma' }],
   options: {},
